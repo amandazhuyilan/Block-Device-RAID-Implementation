1869567e527251da661562bdb97af1a28f44cc86
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-11-28 16:07:21 -0500

    esting failing image

--- mirror-test.c
+++ mirror-test.c
@@ -77,7 +77,8 @@ int main(int arg, char * argv[]){
      printf("Completed Test 4: reads data from the proper location in the images, and doesnt overwrite incorrect locations on write.\n");
 
 	 //Test 5. Continues to read and write correctly after one of the disks fails
-	 image_fail ( disk_1 );
+	 image_fail(disk_1);
+	 printf("failing disk 1\n");
 
 	 char test_write_5 [block_num * BLOCK_SIZE];
 	 char test_read_5 [block_num * BLOCK_SIZE];
