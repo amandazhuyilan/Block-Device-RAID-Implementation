33a70147092829583cb5c62e954674991313da0e
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-11-28 18:44:23 -0500

    edited shell file

--- mirror-test.sh
+++ mirror-test.sh
@@ -2,7 +2,6 @@
 
 # Compile mirror-test.c. use -v -da -Q for debugging in gdb
 
-gcc -g -w -o mirror-test mirror-test.c image.c homework.c -g
 
 
 # Amanda: Create 5120-byte file containing different characters for disk images.
@@ -13,4 +12,6 @@ dd if=/dev/zero bs=512 count=10 | tr '\0' '2' > mirror_disk2.img
 
 dd if=/dev/zero bs=512 count=10 | tr '\0' 'R' > mirror_new_disk.img
 
-./mirror-test /mirror_disk1.img /mirror_disk2.img
+gcc -g -w -o mirror-test mirror-test.c image.c homework.c -g
+
+./mirror-test mirror_disk1.img mirror_disk2.img
