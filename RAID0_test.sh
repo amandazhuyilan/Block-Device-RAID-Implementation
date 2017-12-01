042f46e521876ca71ca99c8e2631e28898fec2fd
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-12-01 17:44:15 -0500

    testing with even number of disks

--- RAID0_test.sh
+++ RAID0_test.sh
@@ -12,14 +12,14 @@ dd if=/dev/zero bs=512 count=1024 | tr '\0' '5' > RAID0_disk5.img
 
 dd if=/dev/zero bs=512 count=1024 | tr '\0' '6' > RAID0_disk6.img
 
-echo "Testing with stripe size = 2 and disk num = 4\n"
+echo "Testing with stripe size = 2 and disk num = 4"
 ./RAID0-test 2 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
 
-echo "Testing with stripe size = 4 and disk num = 4\n"
+echo "Testing with stripe size = 4 and disk num = 4"
 ./RAID0-test 4 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
 
-echo "Testing with stripe size = 7 and disk num = 6\n"
+echo "Testing with stripe size = 7 and disk num = 6"
 ./RAID0-test 7 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img RAID0_disk5.img RAID0_disk6.img
 
-echo "Testing with stripe size = 32 and disk num = 6\n"
+echo "Testing with stripe size = 32 and disk num = 6"
 ./RAID0-test 32 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img RAID0_disk5.img RAID0_disk6.img
