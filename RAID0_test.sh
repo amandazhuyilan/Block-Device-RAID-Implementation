458a4e71f40b9372b4d84b712cba65fe848a6f72
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-12-01 17:10:53 -0500

    updated to sol

--- RAID0_test.sh
+++ RAID0_test.sh
@@ -1,12 +1,12 @@
 gcc -g -w -o RAID0-test RAID0_test.c image.c homework.c -g
 
-dd if=/dev/zero bs=512 count=10 | tr '\0' '1' > RAID0_disk1.img
+dd if=/dev/zero bs=512 count=1024 | tr '\0' '1' > RAID0_disk1.img
 
-dd if=/dev/zero bs=512 count=10 | tr '\0' '2' > RAID0_disk2.img
+dd if=/dev/zero bs=512 count=1024 | tr '\0' '2' > RAID0_disk2.img
 
-dd if=/dev/zero bs=512 count=10 | tr '\0' '3' > RAID0_disk3.img
+dd if=/dev/zero bs=512 count=1024 | tr '\0' '3' > RAID0_disk3.img
 
-dd if=/dev/zero bs=512 count=10 | tr '\0' '4' > RAID0_disk4.img
+dd if=/dev/zero bs=512 count=1024 | tr '\0' '4' > RAID0_disk4.img
 
 
 ./RAID0-test 2 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
