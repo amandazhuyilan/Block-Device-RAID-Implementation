288ab468c6dab44ee6ad2f00975c3c6a891dd70f
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-12-01 12:40:26 -0500

    added RAID 0 and 4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### RAID Implementation in Block Devices
+### Homework 2 Block Devices
 
-In this project, we are implenting RAID 1(mirroring), RAID 0(Stripping) and RAID4 in a block device layer.
+In this assignment, we are implenting RAID 1(mirroring), RAID 0(Stripping) and RAID4 in a block device layer.
 
 #### Some basic provided stuff
 - In the  ```blkdev.h``` header file, ```blkdev``` is structured with a pointer ```*ops```that points to the basic operations of a block device, while the ```private``` points to the block device itself. Therefore whenever we are initalizing a new block device, we reference it as (passing on ```blkdev *dev``` as input) ```struct new_device *new_dev = dev->private```.
@@ -15,10 +15,10 @@ First checks if ```first_blk + num_blks``` exceeds the size of the mirror device
 ```mirror_create(struct blkdev *disks[2])``` copy the contents of both disks assuming the are of the same size (will return error message if not of the same) to a mirror device's disks. Also copies the size of the disk to the mirrored device's ```nblks```.
 
 ```mirror_write(struct blkdev * dev, int first_blk,int num_blks, void *buf)``` writes to both disks of mirror or the remaining good side if the other one has a bad read or is NULL. Will return SUCCESS as long as one read is successful, otherwise return E_UNAVIL.
+
+```mirror_close(struct blkdev *dev)``` closes previously opened non-failed disks, and ```free``` any ```malloc```ed space for blkdev structures.
  
- ```mirror_close(struct blkdev *dev)``` closes previously opened non-failed disks, and ```free``` any ```malloc```ed space for blkdev structures.
- 
- ```mirror_replace(struct blkdev *volume, int i, struct blkdev *newdisk)``` returns ```E_SIZE`` if the size of ```newdisk``` is not as the same size of ```volume```. The non-failing disk need to be replicate using ```read``` to working disk and ```write``` to ```newdisk```. Will return SUCCESS upen coping failed disk to ```newdisk```.
+```mirror_replace(struct blkdev *volume, int i, struct blkdev *newdisk)``` returns ```E_SIZE`` if the size of ```newdisk``` is not as the same size of ```volume```. The non-failing disk need to be replicate using ```read``` to working disk and ```write``` to ```newdisk```. Will return SUCCESS upen coping failed disk to ```newdisk```.
  
  
  #### Test Cases for RAID 1:
@@ -37,9 +37,50 @@ First checks if ```first_blk + num_blks``` exceeds the size of the mirror device
 
 7. Reads and writes (returning data written before first failure) after the other disk fails.
  
- #### Striping (RAID 0):
- ```struct raid0_dev``` is a custom struct for raid0 devices similar to block dev.
- 
- ```int raid0_num_blocks(struct blkdev *dev)``` returns the size of the active stripes in the disk which is an exact multiple of the unit size of a stripe.
- 
- ```struct blkdev *raid0_create(int N, struct blkdev *disks[], int unit)``` creates a raid0 type block device. Returns NULL incase of invalid no. of disks or if the disks are unequal in size.
+#### Striping (RAID 0):
+```struct raid0_dev``` is a custom struct for raid0 devices similar to block dev and image dev.
+
+```struct blkdev *raid0_create(int N, struct blkdev *disks[], int unit)``` creates a raid0 type block device. Returns NULL incase of invalid no. of disks or if the disks are unequal in size.
+
+```int raid0_num_blocks(struct blkdev *dev)``` returns the size of the active stripes in the disk which is an exact multiple of the unit size of a stripe.
+
+```static int raid0_read_write(struct blkdev * dev, int first_blk, int num_blks, void *buf, int isWrite)``` a generic function for both read and write operations into the raid0 type block device. Calculates the disk number and blocks to be read from to written to.
+
+```static int raid0_write(struct blkdev * dev, int first_blk, int num_blks, void *buf)``` calls the generic function ```raid0_read_write``` with write flag enabled.
+
+```static int raid0_read(struct blkdev * dev, int first_blk, int num_blks, void *buf)``` calls the generic function ```raid0_read_write``` with write flag disabled.
+
+```static void raid0_close(struct blkdev *dev)``` closes all underlying discs in this raid0_dev structure. Similar to what was implemented in mirror_close().
+
+
+#### RAID 4:
+```struct raid4_dev``` is a custom struct for raid4 devices similar to block dev and image dev.
+
+```struct blkdev *raid4_create(int N, struct blkdev *disks[], int unit)``` creates a raid4 type block device. Returns NULL incase of invalid no. of disks or if the disks are unequal in size. Initializes the logical construct of parity and data disks into the raid4_dev structure.
+
+```static void raid4_close(struct blkdev *dev)``` closes all underlying discs in this raid4_dev structure. Similar to what was implemented in raid0_close().
+
+```int raid4_num_blocks(struct blkdev *dev)``` returns the size of the active stripes in the data disk (not including the parity disc) which is an exact multiple of the unit size of a stripe.
+
+```static int raid4_read(struct blkdev * dev, int first_blk, int num_blks, void *buf)``` implemented raid4_read using the following logic:
+				read normal:
+					read logic similar to raid0
+				read degrad:
+					recover failed disk's data using XOR on all other disks
+
+```static int raid4_write(struct blkdev *dev, int first_blk, int num_blks, void *buf)``` implemented raid4_write using the following logic:
+				write normal:
+					compute parity 
+					write parity
+					write data on the data disk
+				write degrad:
+					if disk-failed:
+						compute parity with new data, other disk data
+						write parity
+					if disk-normal:
+						recover failed disk's data using XOR on all other disks
+						compute parity using recovered as well as other data
+						write parity
+						write data on to the normal disk
+
+```int raid4_replace(struct blkdev *volume, int i, struct blkdev *newdisk)``` copies computed parity data from the rest of the disks in the RAID4 device into the newdisk and replaces disk i with the newdisk.
--- homework.c
+++ homework.c
@@ -220,21 +220,70 @@ int mirror_replace(struct blkdev *volume, int i, struct blkdev *newdisk)
 
 /**********  RAID0 ***************/
 
-int raid0_num_blocks(struct blkdev *dev)
-{
-    return 0;
+/* Custom struct for raid0 devices similar to
+ * block dev and image dev 
+ */
+struct raid0_dev {
+    struct blkdev **disks;
+    int disksCount;
+    int unitSize;
+    int usableBlocksPerDisk;
+};
+
+int raid0_num_blocks(struct blkdev *dev) {
+    struct raid0_dev *r0dev = dev->private;
+    return r0dev->usableBlocksPerDisk * r0dev->disksCount;
 }
 
-/* read blocks from a striped volume. 
+
+/* Combines raid0's read and write logic */
+static int raid0_read_write(struct blkdev * dev, int first_blk,
+                            int num_blks, void *buf, int isWrite) {
+    struct raid0_dev *r0dev = dev->private;
+
+    //check for bad address errors
+    if (first_blk < 0 || first_blk + num_blks > r0dev->ops->num_blocks(r0dev))
+        return E_BADADDR;
+
+    int unitSize = r0dev->unitSize;
+    int disksCount = r0dev->disksCount;
+    int stripeSize = unitSize * disksCount;
+    int i;
+    for (i = num_blks; i > 0;) {
+        int blockInDisk = (first_blk % unitSize) + (first_blk / stripeSize) * unitSize;
+        int blocksRemaining = unitSize - (blockInDisk % unitSize);
+        int diskNum = (first_blk % stripeSize) / unitSize ;
+
+        int len = (num_blks <= blocksRemaining) ? num_blks : blocksRemaining;
+        struct raid0_dev *diskToUse = r0dev->disks[diskNum];
+        int returnValue;
+        if (isWrite)
+            returnValue = diskToUse->ops->write(diskToUse, blockInDisk, len, buf);
+        else
+            returnValue = diskToUse->ops->read(diskToUse, blockInDisk, len, buf);
+
+        if (returnValue == E_UNAVAIL) {
+            diskToUse->ops->close(diskToUse);
+            return returnValue;
+        } else if (returnValue == SUCCESS) {
+            buf = buf + len * BLOCK_SIZE;
+            i = i - len;
+            first_blk = first_blk + len;
+        } else return returnValue;
+    }
+    return SUCCESS;
+}
+
+
+/* read blocks from a striped volume.
  * Note that a read operation may return an error to indicate that the
  * underlying device has failed, in which case you should (a) close the
  * device and (b) return an error on this and all subsequent read or
  * write operations. 
  */
 static int raid0_read(struct blkdev * dev, int first_blk,
-                       int num_blks, void *buf)
-{
-    return 0;
+                      int num_blks, void *buf) {
+    return raid0_read_write(dev, first_blk, num_blks, *buf, 0);
 }
 
 /* write blocks to a striped volume.
@@ -242,31 +291,101 @@ static int raid0_read(struct blkdev * dev, int first_blk,
  * an error for this and all subsequent read or write operations.
  */
 static int raid0_write(struct blkdev * dev, int first_blk,
-                        int num_blks, void *buf)
-{
-    return 0;
+                       int num_blks, void *buf) {
+    return raid0_read_write(dev, first_blk, num_blks, *buf, 1);
 }
 
 /* clean up, including: close all devices and free any data structures
  * you allocated in stripe_create. 
  */
-static void raid0_close(struct blkdev *dev)
-{
+static void raid0_close(struct blkdev *dev) {
+    struct raid0_dev *r0dev = dev->private;
+    int i;
+    for (i = 0; i < r0dev->disksCount; i++) {
+        struct blkdev *disk = r0dev->disks[i];
+        disk->ops->close(disk);
+    }
+    free(r0dev);
+    free(dev);
 }
+
+
+/* Overriding the basic operations of a block device for raid0 */
+ struct blkdev_ops raid0_ops = {
+     .num_blocks = raid0_num_blocks,
+     .read = raid0_read,
+     .write = raid0_write,
+     .close = raid0_close
+ };
+
+
+/* helper function - checks if the given disks are of same size 
+ * returns 1 on success, -1 otherwise.
+ */
+int checkDisksSameSize(int N, struct blkdev *disks[]) {
+    if (N < 1) {
+        printf("Invalid disk count.\n");
+        return -1;
+    }
+    int i;
+    int num_blocks = disks[0]->ops->num_blocks(disks[0]);
+    for (i = 1; i < N; i++)
+        if (disks[i]->ops->num_blocks(disks[i]) != num_blocks) {
+            printf("Given disks are not of same size.\n");
+            return -1;
+        }
+    return 1;
+}
+
 
 /* create a striped volume across N disks, with a stripe size of
  * 'unit'. (i.e. if 'unit' is 4, then blocks 0..3 will be on disks[0],
  * 4..7 on disks[1], etc.)
  * Check the size of the disks to compute the final volume size, and
  * fail (return NULL) if they aren't all the same.
- * Do not write to the disks in this function.
+ * Do not write to the disks in this function. 
  */
-struct blkdev *raid0_create(int N, struct blkdev *disks[], int unit)
-{
-    return NULL;
+struct blkdev *raid0_create(int N, struct blkdev *disks[], int unit) {
+    if(checkDisksSameSize(N,disks) == -1)
+        return NULL;
+
+    int num_blocks = disks[0]->ops->num_blocks(disks[0]);
+
+    // Saran: Allocate memory after initial check
+    struct blkdev *dev = malloc(sizeof(*dev));
+    struct raid0_dev *r0dev = malloc(sizeof(*r0dev));
+
+    // Saran: Assigning references to the blkdev interface
+    r0dev->disks = disks;
+    r0dev->disksCount = N;
+    r0dev->unitSize = unit;
+    r0dev->usableBlocksPerDisk = (num_blocks / unit) * unit;
+    dev->private = r0dev;
+    dev->ops = &raid0_ops;
+
+    return dev;
 }
 
+
 /**********   RAID 4  ***************/
+
+/* Custom struct for raid4 devices similar to
+ * block dev and image dev 
+ */
+struct raid4_dev {
+    struct blkdev **disks;
+    struct blkdev *parityDisk;
+    int dataDisksCount;
+    int unitSize;
+    int usableBlocksPerDisk;
+    int isDegraded;
+    int failedDiskNumber;
+};
+
+int raid4_num_blocks(struct blkdev *dev) {
+    struct raid4_dev *r4dev = dev->private;
+    return r4dev->usableBlocksPerDisk * r4dev->dataDisksCount;
+}
 
 /* helper function - compute parity function across two blocks of
  * 'len' bytes and put it in a third block. Note that 'dst' can be the
@@ -288,6 +407,59 @@ void parity(int len, void *src1, void *src2, void *dst)
         d[i] = s1[i] ^ s2[i];
 }
 
+
+// INVARIENT: dev->private->failedDiskNumber != -1
+void recoverFailedDiskData(struct blkdev *dev, int blockInDisk, int len, void *outputBuf){
+    struct raid4_dev *r4dev = dev->private;
+    int failedDiskNumber = r4dev->failedDiskNumber;
+
+    // dst = <zeros[len]>
+    memset(outputBuf, '\0', len * BLOCK_SIZE);
+    char tempBuf[len * BLOCK_SIZE];
+    int i;
+    for (i = 0; i < r4dev->dataDisksCount + 1; i++) {
+        if(i != failedDiskNumber){
+            struct blkdev *disk = r4dev->disks[i];
+            result = disk->ops->read(disk, blockInDisk, len, tempBuf);
+            parity(count * BLOCK_SIZE, tempBuf, outputBuf, outputBuf);
+        }
+    }
+}
+
+/* Sets computed parity into parityBuf
+ * Returns the disk num if it fails while computing parity.
+ */
+int computeParity(struct blkdev *dev, int blockInDisk, int len, void *dataForFailedDisk, void *parityBuf){
+    struct raid4_dev *r4dev = dev->private;
+    int parityDiskNum = r4dev->dataDisksCount;
+    int failedDiskNumber = r4dev->failedDiskNumber;
+    
+    // if parity disk is degraded
+    if(r4dev->isDegraded && (r4dev->failedDiskNumber == parityDiskNum)){
+        return parityDiskNum;
+
+    // dst = <zeros[len]>
+    memset(parityBuf, '\0', len * BLOCK_SIZE);
+    char tempBuf[len * BLOCK_SIZE];
+    int i;
+    for (i = 0; i < r4dev->dataDisksCount; i++) {
+        if(failedDiskNumber == i){
+            if(dataForFailedDisk != NULL)
+                memcpy(tempBuf, dataForFailedDisk, len * BLOCK_SIZE);
+            else
+                recoverFailedDiskData(dev, blockInDisk, len, tempBuf);
+        }
+        else{
+            struct blkdev *disk = r4dev->disks[i];
+            result = disk->ops->read(disk, blockInDisk, len, tempBuf);
+            if (result == E_UNAVAIL)
+                return i;
+        }
+        parity(len * BLOCK_SIZE, tempBuf, parityBuf, parityBuf);
+    }
+    return -1;
+}
+
 /* read blocks from a RAID 4 volume.
  * If the volume is in a degraded state you may need to reconstruct
  * data from the other stripes of the stripe set plus parity.
@@ -297,9 +469,55 @@ void parity(int len, void *src1, void *src2, void *dst)
  * close the drive and return an error.
  */
 static int raid4_read(struct blkdev * dev, int first_blk,
-                      int num_blks, void *buf) 
-{
-    return 0;
+                      int num_blks, void *buf) {
+    struct raid4_dev *r4dev = dev->private;
+
+    //check for bad address errors
+    if (first_blk < 0 || first_blk + num_blks > r4dev->ops->num_blocks(r4dev))
+        return E_BADADDR;
+
+    int unitSize = r4dev->unitSize;
+    int disksCount = r4dev->disksCount;
+    int stripeSize = unitSize * disksCount;
+    int i;
+    for (i = num_blks; i > 0;) {
+        int blockInDisk = (first_blk % unitSize) + (first_blk / stripeSize) * unitSize;
+        int blocksRemaining = unitSize - (blockInDisk % unitSize);
+        int diskNum = (first_blk % stripeSize) / unitSize ;
+
+        int len = (num_blks <= blocksRemaining) ? num_blks : blocksRemaining;
+        struct raid0_dev *diskToUse = r4dev->disks[diskNum];
+        int returnValue;
+
+        if(diskToUse != NULL)
+            returnValue = diskToUse->ops->read(diskToUse, blockInDisk, len, buf);
+        else returnValue = E_UNAVAIL;
+
+        if (returnValue == E_UNAVAIL) {
+            // first, close the disk
+            if(diskToUse != NULL)
+                diskToUse->ops->close(diskToUse);   
+            
+            // if the raid4 device is already degraded          
+            if(r4dev->isDegraded){
+                // and if the known failed disk is not this disk, return E_UNAVAIL
+                if(r4dev->failedDiskNumber != diskNum)
+                    return E_UNAVAIL;
+            } else {
+                // marking the raid4 device as degraded
+                r4dev->isDegraded = 1;
+                r4dev->failedDiskNumber = diskNum;
+
+                // and read into buffer the recovered data for the failed disk
+                recoverFailedDiskData(dev, blockInDisk, len, buf);
+            }
+        } else if (returnValue == SUCCESS) {
+            buf = buf + len * BLOCK_SIZE;
+            i = i - len;
+            first_blk = first_blk + len;
+        } else return returnValue;
+    }
+    return SUCCESS;
 }
 
 /* write blocks to a RAID 4 volume.
@@ -314,10 +532,94 @@ static int raid4_read(struct blkdev * dev, int first_blk,
  * In the degraded state perform all writes to non-failed drives, and
  * forget about the failed one. (parity will handle it)
  */
-static int raid4_write(struct blkdev * dev, int first_blk,
-                       int num_blks, void *buf)
-{
-    return 0;
+static int raid4_write(struct blkdev *dev, int first_blk,
+                       int num_blks, void *buf){
+    struct raid4_dev *r4dev = dev->private;
+    struct blkdev *parityDisk = r4dev->parityDisk;
+
+    //check for bad address errors
+    if (first_blk < 0 || first_blk + num_blks > r4dev->ops->num_blocks(r4dev))
+        return E_BADADDR;
+
+    int unitSize = r4dev->unitSize;
+    int disksCount = r4dev->dataDisksCount;
+    int stripeSize = unitSize * dataDisksCount;
+    int i;
+    for (i = num_blks; i > 0;) {
+        int blockInDisk = (first_blk % unitSize) + stripeSetNum * unitSize;
+        int blocksRemaining = unitSize - (blockInDisk % unitSize);
+        int diskNum = (first_blk % stripeSize) / unitSize;
+
+        int len = (num_blks <= blocksRemaining) ? num_blks : blocksRemaining;
+        struct raid4_dev *diskToUse = r4dev->disks[diskNum];
+        int returnValue;   
+
+        if(diskToUse != NULL)
+            returnValue = diskToUse->ops->write(diskToUse, blockInDisk, len, buf);
+        else returnValue = E_UNAVAIL;
+
+        // checking for data disks failures
+        // returning E_UNAVAIL if more than 1 disk fails
+        if(returnValue == E_UNAVAIL) {
+            // first, closing the disk
+            if(diskToUse != NULL)
+                diskToUse->ops->close(diskToUse);  
+
+            // if the raid4 device is already degraded          
+            if(r4dev->isDegraded){
+                // and if the known failed disk is not this disk, return E_UNAVAIL
+                if(r4dev->failedDiskNumber != diskNum)
+                    return E_UNAVAIL;
+            } else {
+                // marking the raid4 device as degraded
+                r4dev->isDegraded = 1;
+                r4dev->failedDiskNumber = diskNum;
+
+                // compute parity with new data, other disk data
+                char parityBuf[len * BLOCK_SIZE];
+                returnValue = computeParity(dev, blockInDisk, len, buf, parityBuf);
+
+                // if disk failed while computing parity
+                if(returnValue == E_UNAVAIL)
+                    return E_UNAVAIL;
+                
+                // write parity
+                returnValue = parityDisk->ops->write(parityDisk, blockInDisk, len, parityBuf);
+
+                // if parity disk failed
+                if(returnValue == E_UNAVAIL)
+                    return E_UNAVAIL;
+            }
+        } else if (returnValue == SUCCESS) {
+            // compute parity using data disk values
+            char parityBuf[len * BLOCK_SIZE];
+            returnValue = computeParity(dev, blockInDisk, len, NULL, parityBuf);
+
+            // if a disk fails while computing parity
+            if(returnValue != -1){
+                r4dev->isDegraded = 1;
+                r4dev->failedDiskNumber = returnValue;
+            }
+
+            // write parity
+            returnValue = parityDisk->ops->write(parityDisk, blockInDisk, len, parityBuf);
+
+            // if parity disk fails
+            if(returnValue == E_UNAVAIL){
+                if(r4dev->isDegraded) return E_UNAVAIL;
+                else{
+                    r4dev->isDegraded = 1;
+                    r4dev->failedDiskNumber = r4dev->dataDisksCount;
+                }
+            }
+
+            // incrementing buffer, iter variable and the block for next iteration
+            buf = buf + len * BLOCK_SIZE;
+            i = i - len;
+            first_blk = first_blk + len;
+        } 
+    }
+    return SUCCESS;
 }
 
 /* clean up, including: close all devices and free any data structures
@@ -325,7 +627,24 @@ static int raid4_write(struct blkdev * dev, int first_blk,
  */
 static void raid4_close(struct blkdev *dev)
 {
+    struct raid4_dev *r4dev = dev->private;
+    int i;
+    for (i = 0; i < r4dev->dataDisksCount + 1; i++) {
+        struct blkdev *disk = r4dev->disks[i];
+        disk->ops->close(disk);
+    }
+    free(r4dev);
+    free(dev);
 }
+
+/* Overriding the basic operations of a block device for raid0 */
+ struct blkdev_ops raid4_ops = {
+     .num_blocks = raid4_num_blocks,
+     .read = raid4_read,
+     .write = raid4_write,
+     .close = raid4_close
+ };
+
 
 /* Initialize a RAID 4 volume with strip size 'unit', using
  * disks[N-1] as the parity drive. Do not write to the disks - assume
@@ -335,8 +654,26 @@ static void raid4_close(struct blkdev *dev)
  */
 struct blkdev *raid4_create(int N, struct blkdev *disks[], int unit)
 {
-    return NULL;
+    if(checkDisksSameSize(N,disks) == -1)
+        return NULL;
+
+    int num_blocks = disks[0]->ops->num_blocks(disks[0]);
+    struct blkdev *dev = malloc(sizeof(*dev));
+    struct raid4_dev *r4dev = malloc(sizeof(*r4dev));
+
+    r4dev->disks = disks;
+    r4dev->parityDisk = disks[N-1];
+    r4dev->dataDisksCount = N-1;
+    r4dev->usableBlocksPerDisk = (num_blocks / unit) * unit;
+    r4dev->unitSize = unit;
+    r4dev->isDegraded = 0;
+    r4dev->failedDiskNumber = -1;
+    dev->private = r4dev;
+    dev->ops = &raid4_ops;
+
+    return dev;
 }
+
 
 /* replace failed device 'i' in a RAID 4. Note that we assume
  * the upper layer knows which device failed. You will need to
@@ -345,6 +682,28 @@ struct blkdev *raid4_create(int N, struct blkdev *disks[], int unit)
  */
 int raid4_replace(struct blkdev *volume, int i, struct blkdev *newdisk)
 {
+    struct raid4_dev *r4dev = volume->private;
+    struct blkdev *diskToBeReplaced = r4dev->disks[i];
+
+    // setting the disk to be replaced as the failed disk
+    r4dev->failedDiskNumber = i;
+
+    int r4DiskBlockCount = diskToBeReplaced->ops->num_blocks(diskToBeReplaced);
+    if (newdisk->ops->num_blocks(newdisk) != r4DiskBlockCount)
+        return E_SIZE;
+
+    // Compute parity from other disks and store in new disk
+    // irrespective of it being a data drive or a parity drive
+    char recoveredDataBuf[r4DiskBlockCount * BLOCK_SIZE];
+    recoverFailedDiskData(dev, 0, r4DiskBlockCount, recoveredDataBuf);
+
+    // write computed data into the newdisk
+    newdisk->ops->write(newdisk, 0, r4DiskBlockCount, recoveredDataBuf);
+
+    // Closing the disk to be replaced
+    diskToBeReplaced->ops->close(diskToBeReplaced);
+    r4dev->disks[i] = newdisk;
+
     return SUCCESS;
 }
 
