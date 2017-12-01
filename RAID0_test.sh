c86c403ea32e2ae506b0526dfb4df8caf80147ba
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-12-01 17:14:45 -0500

    edited create func name

--- RAID0_test.sh
+++ RAID0_test.sh
@@ -8,11 +8,13 @@ dd if=/dev/zero bs=512 count=1024 | tr '\0' '3' > RAID0_disk3.img
 
 dd if=/dev/zero bs=512 count=1024 | tr '\0' '4' > RAID0_disk4.img
 
+dd if=/dev/zero bs=512 count=1024 | tr '\0' '5' > RAID0_disk5.img
 
-./RAID0-test 2 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
 
-./RAID0-test 4 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
+./RAID0-test 2 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img RAID0_disk5.img
 
-./RAID0-test 7 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
+./RAID0-test 4 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img RAID0_disk5.img
 
-./RAID0-test 32 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img 
+./RAID0-test 7 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img RAID0_disk5.img
+
+./RAID0-test 32 RAID0_disk1.img RAID0_disk2.img RAID0_disk3.img RAID0_disk4.img RAID0_disk5.img
