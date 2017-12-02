ed760b720f580fe8543032e6d0075471d2bb6191
Amanda Zhu <amandazhu@Amandas-MacBook-Pro.local>
2017-12-01 21:46:09 -0500

    completed all tests for RAID4

--- RAID4_test.c
+++ RAID4_test.c
@@ -32,21 +32,21 @@ int main(int argc, char **argv)
 
     int nblks = disks[0]->ops->num_blocks(disks[0]);
     nblks = nblks - (nblks % stripe_size);
-    assert(RAID_4->ops->num_blocks(RAID_4) == num_disks*nblks);
+    assert(RAID_4->ops->num_blocks(RAID_4) == (num_disks -1)*nblks);
 
     printf("Passed test 2: Reports the correct size.\n");
 
     int chunk = stripe_size * BLOCK_SIZE;
     //char *buf = malloc(num_disks * chunk);
-    char *buf = calloc((num_disks) * chunk + 100, 1);
+    char *buf = calloc((num_disks-1) * chunk + 100, 1);
 
-    for (i = 0; i < num_disks; i++)
+    for (i = 0; i < num_disks-1; i++)
         memset(buf + i * chunk, 'A'+i, chunk);
 
     int result;
     for (i = 0; i < 10; i++) {
-        result = RAID_4->ops->write(RAID_4, i*num_disks*stripe_size,
-                                     num_disks*stripe_size, buf);
+        result = RAID_4->ops->write(RAID_4, i*(num_disks -1)*stripe_size,
+                                     (num_disks-1)*stripe_size, buf);
         assert(result == SUCCESS);
     }
 
@@ -56,11 +56,11 @@ int main(int argc, char **argv)
         result = RAID_4->ops->read(RAID_4, i*num_disks*stripe_size,
                                     num_disks*stripe_size, buf2);
         assert(result == SUCCESS);
-        assert(memcmp(buf, buf2, num_disks * chunk) == 0);
+        assert(memcmp(buf, buf2, (num_disks -1) * chunk) == 0);
     }
 
     int j;
-  	for (i = 0; i < num_disks; i++) {
+  	for (i = 0; i < (num_disks -1); i++) {
    		for (j = 0; j < 10; j++) {
    			result = disks[i]->ops->read(disks[i], i*stripe_size,
                                      stripe_size, buf2);
@@ -71,46 +71,46 @@ int main(int argc, char **argv)
 
     printf("Passed test 3: Writes and read back back data from the right disks and locations\n.");
 
-    for (i = 0; i < num_disks; i++)
+    for (i = 0; i < (num_disks -1); i++)
         memset(buf + i * chunk, 'a'+i, chunk);
     
     for (i = 0; i < 8; i++) {
-        for (j = 0; j < num_disks*stripe_size; j ++) {
-            result = RAID_4->ops->write(RAID_4, i*num_disks*stripe_size + j, 1,
+        for (j = 0; j < (num_disks -1)*stripe_size; j ++) {
+            result = RAID_4->ops->write(RAID_4, i*(num_disks-1)*stripe_size + j, 1,
                                          buf + j*BLOCK_SIZE);
             assert(result == SUCCESS);
         }
     }
 
     for (i = 0; i < 8; i++) {
-        result = RAID_4->ops->read(RAID_4, i*num_disks*stripe_size,
-                                    num_disks*stripe_size, buf2);
+        result = RAID_4->ops->read(RAID_4, i*(num_disks-1)*stripe_size,
+                                    (num_disks-1)*stripe_size, buf2);
         assert(result == SUCCESS);
-        assert(memcmp(buf, buf2, num_disks * chunk) == 0);
+        assert(memcmp(buf, buf2, (num_disks -1) * chunk) == 0);
     }
 
     printf("Passed small writes. \n");
 
-        char *big = malloc(5 * num_disks*chunk);
+        char *big = malloc(5 * (num_disks -1)*chunk +100);
     for (i = 0; i < 5; i++)
-        for (j = 0; j < num_disks; j++)
-            memset(big + j * chunk + i*num_disks*chunk, 'f'+i, chunk);
+        for (j = 0; j < (num_disks -1); j++)
+            memset(big + j * chunk + i*(num_disks -1)*chunk, 'f'+i, chunk);
 
-    int offset = num_disks*stripe_size / 2;
-    result = RAID_4->ops->write(RAID_4, offset, 5*num_disks*stripe_size, big);
+    int offset = (num_disks -1) * stripe_size / 2;
+    result = RAID_4->ops->write(RAID_4, offset, 5*(num_disks -1)*stripe_size, big);
     assert(result == SUCCESS);
 
-    char *big2 = malloc(5 * num_disks*chunk);
-    result = RAID_4->ops->read(RAID_4, offset, 5*num_disks*stripe_size, big2);
+    char *big2 = malloc(5 * (num_disks -1)*chunk + 100);
+    result = RAID_4->ops->read(RAID_4, offset, 5*(num_disks -1)*stripe_size, big2);
     assert(result == SUCCESS);
-    assert(memcmp(big, big2, 5 * num_disks * chunk) == 0);
+    assert(memcmp(big, big2, 5 * (num_disks -1) * chunk) == 0);
 
 
     result = RAID_4->ops->read(RAID_4, 0, offset, buf2);
     assert(result == SUCCESS);
     assert(memcmp(buf, buf2, offset*BLOCK_SIZE) == 0);
 
-    result = RAID_4->ops->read(RAID_4, 5*num_disks*stripe_size + offset, offset, buf2);
+    result = RAID_4->ops->read(RAID_4, 5*(num_disks -1)*stripe_size + offset, offset, buf2);
     assert(result == SUCCESS);
     assert(memcmp(buf + offset*BLOCK_SIZE, buf2, offset*BLOCK_SIZE) == 0);
 
@@ -118,32 +118,58 @@ int main(int argc, char **argv)
 
    	image_fail(disks[0]);
 
-        result = RAID_4->ops->read(RAID_4, offset, 5*num_disks*stripe_size, big2);
+    result = RAID_4->ops->read(RAID_4, offset, 5*(num_disks-1)*stripe_size, big2);
+    assert(result == SUCCESS);
+    assert(memcmp(big, big2, 5*(num_disks-1)*chunk) == 0);
+
+    for (i = 0; i < (num_disks -1); i++)
+        memset(buf + i * chunk, 'g'+i, chunk);
+
+    for (i = 0; i < 10; i++) {
+        result = RAID_4->ops->write(RAID_4, i*(num_disks -1)*stripe_size,
+                                  (num_disks -1)*stripe_size, buf);
         assert(result == SUCCESS);
-        assert(memcmp(big, big2, 5*num_disks*chunk) == 0);
-    
-        for (i = 0; i < num_disks; i++)
-            memset(buf + i * chunk, 'g'+i, chunk);
+    }
 
-        for (i = 0; i < 16; i++) {
-            result = RAID_4->ops->write(RAID_4, i*num_disks*stripe_size,
-                                      num_disks*stripe_size, buf);
-            assert(result == SUCCESS);
-        }
+    for (i = 0; i < 10; i++) {
+        memset(buf2, 0, (num_disks -1)*stripe_size*BLOCK_SIZE);
+        result = RAID_4->ops->read(RAID_4, i*(num_disks-1)*stripe_size,
+                                 (num_disks-1)*stripe_size, buf2);
+        assert(result == SUCCESS);
+        assert(memcmp(buf, buf2, (num_disks-1) * chunk) == 0);
+    }
 
-        for (i = 0; i < 10; i++) {
-            memset(buf2, 0, num_disks*stripe_size*BLOCK_SIZE);
-            result = RAID_4->ops->read(RAID_4, i*num_disks*stripe_size,
-                                     num_disks*stripe_size, buf2);
-            assert(result == SUCCESS);
-            assert(memcmp(buf, buf2, num_disks * chunk) == 0);
-        }
+    printf("Passed test 5: Continues to read and write correctly after one of the disks fails");
 
-        printf("Passed test 5: Continues to read and write correctly after one of the disks fails");
+     struct blkdev *new_disk = disks[num_disks-1];
+	 result = raid4_replace(RAID_4, 0, new_disk);
+	        assert(result == SUCCESS);
+	    
+	        for (i = 0; i < 10; i++) {
+	            memset(buf2, 0, num_disks*stripe_size*BLOCK_SIZE);
+	            result = RAID_4->ops->read(RAID_4, i*(num_disks-1)*stripe_size,
+	                                     (num_disks-1)*stripe_size, buf2);
+	            assert(result == SUCCESS);
+	            assert(memcmp(buf, buf2, (num_disks-1) * chunk) == 0);
+	        }
+
+	        // create a new disk for replacement
+	        image_fail(disks[1]);
+	        buf[0] = 'h';
+	        for (i = 0; i < 10; i++) {
+	            RAID_4->ops->write(RAID_4, i*(num_disks-1)*stripe_size,
+	                             (num_disks-1)*stripe_size, buf);
+	            assert(result == SUCCESS);
+	        }
+	    
+	        for (i = 0; i < 10; i++) {
+	            result = RAID_4->ops->read(RAID_4, i*(num_disks-1)*stripe_size,
+	                                     (num_disks-1)*stripe_size, buf2);
+	            assert(result == SUCCESS);
+	            assert(memcmp(buf, buf2, (num_disks-1) * chunk) == 0);
+	        }
+
+	   	printf("RAID4 Test: SUCCESS\n");
+	    }
 
 
-
-
-
-
-}
